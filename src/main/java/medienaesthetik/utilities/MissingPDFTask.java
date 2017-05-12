7c570644d8251f5ceefd19d098f6b43aa7c969a3
knob1 <knob1de@gmail.com>
2017-05-12 19:45:13 +0200

    erweiterung des MissingPDFTasks

--- src/main/java/medienaesthetik/utilities/MissingPDFTask.java
+++ src/main/java/medienaesthetik/utilities/MissingPDFTask.java
@@ -1,11 +1,15 @@
 package medienaesthetik.utilities;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.TimerTask;
+import java.util.concurrent.TimeUnit;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
+import com.google.common.io.Files;
 
 public class MissingPDFTask extends TimerTask {
 	private static final Logger logger = LogManager.getLogger("MissingPDFTask");
@@ -14,6 +18,7 @@ public class MissingPDFTask extends TimerTask {
 	public void run() {
 		File[] OCRFiles = new File(ConfigHandler.getInstance().getValue("document_bearb.path")).listFiles();
 		File[] allFiles = new File(ConfigHandler.getInstance().getValue("document_original.path")).listFiles();
+		int copyCount = 0;
 		
 		ArrayList<String> MissingOCRFiles = new ArrayList<String>();
 		
@@ -37,11 +42,61 @@ public class MissingPDFTask extends TimerTask {
 			}
 		}
 		
+		for(String filename : MissingOCRFiles){
+			if(filename.contains("pdf")){
+				File from = new File(ConfigHandler.getInstance().getValue("document_original.path") + "/" + filename);
+				File to = new File(ConfigHandler.getInstance().getValue("tempfolder.path") + "/" + filename);
+				try {
+					Files.copy(from, to);
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+				from.delete();
+				copyCount = copyCount + 1;
+			}
+		}
+		
+		try {
+			TimeUnit.SECONDS.sleep(5);
+		} catch (InterruptedException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		
+		File[] tempFiles = new File(ConfigHandler.getInstance().getValue("tempfolder.path")).listFiles();
+		for(File file : tempFiles){
+			if(!file.getName().contains("DS_Store")){
+				File from = new File(ConfigHandler.getInstance().getValue("tempfolder.path") + "/" + file.getName());
+				File to = new File(ConfigHandler.getInstance().getValue("document_original.path") + "/" + file.getName());
+				try {
+					Files.copy(from, to);
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+				from.delete();
+				logger.info(from.getName() + " wurde erneut zum OCR Prozess gesendet.");
+			}
+		}	
+		
 		if(MissingOCRFiles.isEmpty()){
 			WriteToFilemakerField.getInstance().write(ConfigHandler.getInstance().getValue("filemaker.status.file.portal"), ConfigHandler.getInstance().getValue("filemaker.status.layout"), "MissingPDF_result", "---");
 		}
 		else {
 			WriteToFilemakerField.getInstance().write(ConfigHandler.getInstance().getValue("filemaker.status.file.portal"), ConfigHandler.getInstance().getValue("filemaker.status.layout"), "MissingPDF_result", MissingOCRFiles.toString());
 		}
+	}
+	
+	private int getTempFolderItemLength(){
+		//Without DS_Store
+		int numberOfItems = 0;
+		File[] tempFiles = new File(ConfigHandler.getInstance().getValue("tempfolder.path")).listFiles();
+		
+		for(File file : tempFiles){
+			if(!file.getName().contains("DS_Store")){
+				numberOfItems = numberOfItems + 1; 
+			}
+		}
+		
+		return numberOfItems;
 	}
 }
